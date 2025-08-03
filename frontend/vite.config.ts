8c6f3d70965f310d3089e36f83cf2496ff957c29
Desmond Garrido <desmond.garrido@gmail.com>
2025-08-03 16:15:30 -0700

    docker changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     proxy: {
       '/api': 'http://backend:8080',   
       '/ws': {
